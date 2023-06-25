342fc7da2ded87c7b0841a1734cc037fc0cb4d93
Abi Noval Fauzi <novalabi612@gmail.com>
2023-06-25 07:18:15 +0700

    feat: login validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -31,7 +31,7 @@ class AuthController extends Controller
         }
 
         return back()->withErrors([
-            'email' => 'The provided credentials do not match our records.',
+            'login' => 'Kredensial tidak ditemukan.',
         ])->onlyInput('email');
     }
 
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,16 +2,27 @@
     <form action="{{ route('authenticate') }}" method="POST" class="max-w-sm bg-white p-8 rounded-lg mx-auto">
         @csrf
         <h1 class="font-black text-2xl mb-4">Selamat Datang Kembali!</h1>
+        @error('login')
+            <div class="py-2 px-4 rounded bg-red-100 text-red-600 mb-4 border border-red-600">
+                {{ $message }}
+            </div>
+        @enderror
         <div class="grid gap-4">
-            <div class="grid">
-                <label for="email">Email</label>
+            <div class="grid gap-1">
+                <label for="email" class="font-medium text-zinc-800">Email</label>
                 <input type="email" name="email" id="email"
                     class="py-2 px-4 rounded border focus:outline-none">
+                @error('email')
+                    <span class="text-red-600 text-sm font-medium">{{ $message }}</span>
+                @enderror
             </div>
-            <div class="grid">
-                <label for="password">Password</label>
+            <div class="grid gap-1">
+                <label for="password" class="font-medium text-zinc-800">Password</label>
                 <input type="password" name="password" id="password"
                     class="py-2 px-4 rounded border focus:outline-none">
+                @error('password')
+                    <span class="text-red-600 text-sm font-medium">{{ $message }}</span>
+                @enderror
             </div>
             <button class="py-2 font-bold px-6 rounded bg-indigo-600 text-white">Login</button>
             <div class="grid grid-cols-2 gap-2 items-center">
